badaffb22fd90cd39e893c23e0fd641ab5c262b4
rachgonz <rg48794n@pace.edu>
2017-07-07 01:21:07 -0400

    Add files via upload

---
+++ showhide.js
@@ -0,0 +1,13 @@
+$('#show').click(function()
+{
+	$('#show').css('display','none');
+	$('#data').show();
+	$('#hide').show();
+});
+
+$('#hide').click(function()
+{
+	$('#hide').css('display','none');
+	$('#data').hide();
+	$('#show').show();
+});
